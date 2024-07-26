90c347688f4fdbd3e5812220f0188aec24cda416
lbgabriel <146462289+lbgabriel@users.noreply.github.com>
2024-07-26 12:52:17 -0600

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,7 +1,7 @@
-#from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LinearRegression
-#from sklearn.metrics import mean_squared_error, r2_score
-#from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.impute import SimpleImputer
 #import streamlit as st
 
 
